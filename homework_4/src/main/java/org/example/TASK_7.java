8420c90b73479514866717684fde348f9a72cb49
Serhii Shparhalenko <serhigold7@gmail.com>
2023-05-31 13:03:22 +0200

    new commit homework

--- homework_4/src/main/java/org/example/TASK_7.java
+++ homework_4/src/main/java/org/example/TASK_7.java
@@ -11,12 +11,13 @@ public class TASK_7 {
 
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-
+        //Если нам нужно вывести 3 значное число тогда
         if (x >= 101 && x > 0) {
             //уменьшаем и выводим
             --x;
             System.out.println(x);
             System.out.println("число удовлетворяет условия");
+            // 2 значное - false
         } else if (x <= 100 && x > 0) {
             //уменьшаем и выводим
             --x;
