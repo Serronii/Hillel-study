a0e3a659b89459d792880d6299dc184e2fdf9d4e
Serhii Shparhalenko <serhigold7@gmail.com>
2023-06-26 15:25:01 +0200

    commit lesson 10

---
+++ First_project/src/main/java/lesson_10_Builder_regex_OOP/MainRegex.java
@@ -0,0 +1,14 @@
+package lesson_10_Builder_regex_OOP;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainRegex {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[a-z]",Pattern.CASE_INSENSITIVE); // задаем условие проверки (регекс выражение)
+        Matcher matcher = pattern.matcher("Visit Java!"); // задаем елемент для проверки (визит джава)
+        System.out.println(matcher.find()); // проверка тру фолс
+
+
+    }
+}
---
+++ First_project/src/main/java/lesson_10_String_Buffer_String_Builder/BOOTLE/Gin.java
@@ -0,0 +1,13 @@
+package lesson_10_String_Buffer_String_Builder.BOOTLE;
+
+
+// Для того чтобы унаследовать состояние для Обьекта нужно сделать екстендс Класа (можем унаследовать только 1 клас)
+public class Gin extends AlcoholBoottle {
+
+    int year;
+    String type;
+    String [] ingredients;
+
+
+
+}
