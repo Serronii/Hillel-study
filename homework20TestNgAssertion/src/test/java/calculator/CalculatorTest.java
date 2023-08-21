ef972458ee279d1e80d9e717de57d9ded9e79904
Serhii Shparhalenko <serhigold7@gmail.com>
2023-08-21 13:43:53 +0200

    Commit 20 homework

--- homework20TestNgAssertion/src/test/java/calculator/CalculatorTest.java
+++ homework20TestNgAssertion/src/test/java/calculator/CalculatorTest.java
@@ -1,4 +1,5 @@
 package calculator;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CalculatorTest {
@@ -7,5 +8,13 @@ public class CalculatorTest {
 
 
     @Test
+    public void sumTest(){
+        int a = 10;
+        int b = 2;
+        int result = 12;
+        int actualResult = calculator.sum(a, b);
+        Assert.assertEquals(result, actualResult);
+        System.out.println("Sum test a + b");
+    }
 
 }
